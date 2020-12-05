8915d46b062b9ec08a93f9966a1b75ec2b253514
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-04 20:00:58 -0800

    Update index to utilize config and middleware functionality

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const users = require('./routes/users');
 const recipes = require('./routes/recipes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const config = require('config');
+const errorHandler = require('./middleware/error');
 const express = require('express');
 const app = express();
 
@@ -10,19 +12,20 @@ app.use(cors());
 app.use(express.json());
 
 // When deploy. Use a connection string for production environment.
-mongoose.connect('mongodb://localhost/easymealplanner')
+const db = config.get('db');
+mongoose.connect(db)
     .then(() => console.log('Successfully connected to MongoDB...'))
     .catch(() => console.log('Unable to connect to MongoDB.'));
-
-
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 app.use('/api/recipes', recipes);
-
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log('Listening on port 5000');
 });
+
+module.exports = server;
